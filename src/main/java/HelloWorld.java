09ffb717dd689c3af1ada79699eff08bc250932c
Rui Rijo <rui.rijo@ipleiria.pt>
2025-02-24 13:48:53 +0000

    Rui Rijo

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
+    private JLabel lblMensagem2_rui_rijo;
+
 
     public HelloWorld(){
         setContentPane(panel1);
